f2c13e6d76cf46c975ccdab0c893fc3f04d47de4
Isaac Yochelson <isaac.yochelson@rutgers.edu>
2013-11-13 10:51:52 -0500

    hardcodetest is now working as intended to perform pick and place in our controlled workspace.

--- src/hardcodetest.cpp
+++ src/hardcodetest.cpp
@@ -16,10 +16,9 @@
 
 using namespace std;
 
-void fillMap(map<string, double> &goal, string filename);
-void savePlan(string filename, moveit::planning_interface::MoveGroup::Plan* plan);
-string input = "test.txt";
-string output = "testingPlanSaver.trj";
+void moveToPose(string joint, move_group_interface::MoveGroup& group, geometry_msgs::Pose* pose);
+void pick (int cup, move_group_interface::MoveGroup& group, string joint);
+void place (int cup, move_group_interface::MoveGroup& group, string joint);
 
 int main(int argc, char** argv)
 {
@@ -28,32 +27,81 @@ int main(int argc, char** argv)
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 	// this connecs to a running instance of the move_group node
-	move_group_interface::MoveGroup group("left_arm");
-	group.setPlannerId("PRMstarkConfigDefault");
-	group.setStartStateToCurrentState();
+	move_group_interface::MoveGroup leftGroup("left_arm");
+	move_group_interface::MoveGroup rightGroup("right_arm");
+	leftGroup.setPlannerId("PRMstarkConfigDefault");
+	leftGroup.setStartStateToCurrentState();
+	rightGroup.setPlannerId("PRMstarkConfigDefault");
+	rightGroup.setStartStateToCurrentState();
 	ROS_INFO("setting target");
-	geometry_msgs::Pose pose;
-	pose.position.x = 0.82;
-	pose.position.y = 0.52;
-	pose.position.z = 0.35;
-	pose.orientation.x = 0;
-	pose.orientation.y = 1;
-	pose.orientation.z = 0;
-	pose.orientation.w = 0;
-	group.setPoseTarget(pose, "left_wrist");
-	moveit::planning_interface::MoveGroup::Plan plan;
-	ROS_INFO("requesting plan.");
-	bool gotPlan = group.plan(plan);
-	if (gotPlan) {
-		ROS_INFO("plan received.");
-		group.execute(plan);
-		ROS_INFO("plan execution completed.");
+	pick(5, leftGroup, "left_wrist");
+	place(0, leftGroup, "left_wrist");
+	pick(-3, rightGroup, "right_wrist");
+	place(0, rightGroup, "right_wrist");
+	pick(2, leftGroup, "left_wrist");
+	place(0, leftGroup, "left_wrist");
+	pick(0, rightGroup, "right_wrist");
+	place(-3, rightGroup, "right_wrist");
+	return 0;
+}
+void pick (int cup, move_group_interface::MoveGroup& group, string joint) {
+	string arm;
+	if (joint == "left_wrist") {
+		arm = "left";
+	} else {
+		arm = "right";
 	}
-	system("python /home/pracsys/workspaces/hydro_ws/src/drogon_interface/src/gripper.py open left");
-	pose.position.x = 0.82;
-	pose.position.y = 0.52;
-	pose.position.z = 0.14;
-	group.setPoseTarget(pose, "left_wrist");
+	geometry_msgs::Pose* pose = new geometry_msgs::Pose;
+	pose->position.x = 0.82;
+	pose->position.y = 0.104 * cup;
+	pose->position.z = 0.35;
+	pose->orientation.x = 0;
+	pose->orientation.y = 1;
+	pose->orientation.z = 0;
+	pose->orientation.w = 0;
+	moveToPose(joint, group, pose);
+	pose->position.z = 0.14;
+	moveToPose(joint, group, pose);
+	stringstream ss;
+	ss << "python /home/pracsys/workspaces/hydro_ws/src/drogon_interface/src/gripper.py close " << arm;
+	system(ss.str().c_str());
+	pose->position.z = 0.35;
+	moveToPose(joint, group, pose);
+}
+void place (int cup, move_group_interface::MoveGroup& group, string joint) {
+	string arm;
+	if (joint == "left_wrist") {
+		arm = "left";
+	} else {
+		arm = "right";
+	}
+	geometry_msgs::Pose* pose = new geometry_msgs::Pose;
+	pose->position.x = 0.82;
+	pose->position.y = 0.104 * cup;
+	pose->position.z = 0.35;
+	pose->orientation.x = 0;
+	pose->orientation.y = 1;
+	pose->orientation.z = 0;
+	pose->orientation.w = 0;
+	moveToPose(joint, group, pose);
+	pose ->position.z = 0.19;
+	moveToPose(joint, group, pose);
+	stringstream ss;
+	ss << "python /home/pracsys/workspaces/hydro_ws/src/drogon_interface/src/gripper.py open " << arm;
+	system(ss.str().c_str());
+	pose->position.z = 0.35;
+	moveToPose(joint, group, pose);
+	if (arm == "left") {
+		pose->position.y = 0.25;
+	} else {
+		pose->position.y = -0.25;
+	}
+	moveToPose(joint, group, pose);
+}
+void moveToPose(string joint, move_group_interface::MoveGroup& group, geometry_msgs::Pose* pose) {
+	bool gotPlan;
+	moveit::planning_interface::MoveGroup::Plan plan;
+	group.setPoseTarget(*pose, joint);
 	ROS_INFO("requesting plan.");
 	gotPlan = group.plan(plan);
 	if (gotPlan) {
@@ -62,40 +110,4 @@ int main(int argc, char** argv)
 		group.execute(plan);
 		ROS_INFO("plan execution completed.");
 	}
-	system("python /home/pracsys/workspaces/hydro_ws/src/drogon_interface/src/gripper.py close left");
-	pose.position.x = 0.82;
-	pose.position.y = 0.52;
-	pose.position.z = 0.35;
-	group.setPoseTarget(pose, "left_wrist");
-	ROS_INFO("requesting plan.");
-	gotPlan = group.plan(plan);
-	if (gotPlan) {
-		ROS_INFO("plan received.");
-		group.execute(plan);
-		ROS_INFO("plan execution completed.");
-	}
-	pose.position.x = 0.82;
-	pose.position.y = 0;
-	pose.position.z = 0.35;
-	group.setPoseTarget(pose, "left_wrist");
-	ROS_INFO("requesting plan.");
-	gotPlan = group.plan(plan);
-	if (gotPlan) {
-		ROS_INFO("plan received.");
-		group.execute(plan);
-		ROS_INFO("plan execution completed.");
-	}
-	pose.position.x = 0.82;
-	pose.position.y = 0;
-	pose.position.z = 0.19;
-	group.setPoseTarget(pose, "left_wrist");
-	ROS_INFO("requesting plan.");
-	gotPlan = group.plan(plan);
-	if (gotPlan) {
-		ROS_INFO("plan received.");
-		group.execute(plan);
-		ROS_INFO("plan execution completed.");
-	}
-	system("python /home/pracsys/workspaces/hydro_ws/src/drogon_interface/src/gripper.py open left");
-	return 0;
 }
---
+++ src/workspace.cpp
@@ -0,0 +1,11 @@
+/* 
+ * File:   iktest.cpp
+ * Author: Isaac Yochelson
+ *
+ * Created on November 13, 2013, 10:48 AM
+ */
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv)
