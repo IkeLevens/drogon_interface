ddea6de55eba0a62ee28e58d811d2e2d0ebade70
Isaac Yochelson <isaac.yochelson@rutgers.edu>
2013-11-22 13:42:05 -0500

    Added a new executable, curves, which plans to a series of points in a comma sepparated values text file and then produces a trajectory comma separated values output file.

---
+++ src/curves.cpp
@@ -0,0 +1,147 @@
+/* 
+ * File:   curves.cpp
+ * Author: Isaac Yochelson
+ *
+ * Created on August 22, 2013, 10:30 AM
+ */
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit_msgs/RobotTrajectory.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+#include <geometry_msgs/Pose.h>
+#include <baxter_msgs/EndpointState.h>
+#include <cstdlib>			//standard library for C/C++
+#include <iostream>
+#include <fstream>
+#include <boost/tokenizer.hpp>
+
+using namespace std;
+
+string filename = "trajectory_curved_left - 8.csv";
+string output = "trajectory_curved_left - 8.trj";
+ofstream planOutput;
+double addTime = 0;
+
+moveit::planning_interface::MoveGroup::Plan planToPose(string joint, move_group_interface::MoveGroup& group, geometry_msgs::Pose* pose);
+void openPlan(string filename, moveit::planning_interface::MoveGroup::Plan plan);
+void savePlan(moveit::planning_interface::MoveGroup::Plan plan);
+void closePlan();
+void fillMap(map<string, vector<double> > &goal, string filename);
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "Test_Node");
+	// start a ROS spinning thread
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+	
+	// Create MoveGroup instances for each arm, set them to PRM*, and create a pointer to be assigned
+	// to the correct arm to be used for each target.
+	move_group_interface::MoveGroup leftGroup("left_arm");
+	move_group_interface::MoveGroup rightGroup("right_arm");
+	leftGroup.setPlannerId("PRMstarkConfigDefault");
+	leftGroup.setStartStateToCurrentState();
+	rightGroup.setPlannerId("PRMstarkConfigDefault");
+	rightGroup.setStartStateToCurrentState();
+	move_group_interface::MoveGroup* group;
+	group = &leftGroup;
+	string joint = "left_wrist";
+
+	moveit::planning_interface::MoveGroup::Plan plan;
+	map<string, vector<double> > targetMap;
+	fillMap(targetMap, filename);
+	geometry_msgs::Pose target;
+	target.orientation.x = 0;
+	target.orientation.y = 1;
+	target.orientation.z = 0;
+	target.orientation.w = 0;
+
+	vector<double>::iterator xiter = targetMap["x"].begin();	
+	vector<double>::iterator yiter = targetMap["y"].begin();
+	vector<double>::iterator ziter = targetMap["z"].begin();
+	for (xiter= targetMap["x"].begin(); xiter != targetMap["x"].end(); ++xiter) {
+		target.position.x = *xiter;
+		target.position.y = *yiter;
+		target.position.z = *ziter;
+		++yiter;
+		++ziter;
+		plan = planToPose(joint, *group, &target);
+		if (xiter == targetMap["x"].begin()) {
+			openPlan(output, plan);
+		}
+		savePlan(plan);
+	}
+	closePlan();
+	return 0;
+}
+moveit::planning_interface::MoveGroup::Plan planToPose(string joint, move_group_interface::MoveGroup& group, geometry_msgs::Pose* pose)
+{
+	bool gotPlan;
+	moveit::planning_interface::MoveGroup::Plan plan;
+	group.setPoseTarget(*pose, joint);
+	ROS_INFO("requesting plan.");
+	gotPlan = group.plan(plan);
+	if (gotPlan) {
+		ROS_INFO("plan received.");
+//		cout << plan.trajectory_ << endl;
+		group.execute(plan);
+		ROS_INFO("plan execution completed.");
+	}
+	return plan;
+}
+void fillMap(map<string, vector<double> > &goals, string filename)
+{
+	ROS_INFO("filling map");
+	ifstream goalInput;
+	cout << filename.c_str() << endl;
+	goalInput.open(filename.c_str());
+	double value;
+	string keyLine;
+	string valueLine;
+	getline(goalInput, keyLine);
+	cout << keyLine << endl;
+	typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+	boost::char_separator<char> commaDelimited(",");
+	tokenizer keys(keyLine, commaDelimited);
+	while (getline(goalInput, valueLine)) {
+		tokenizer values(valueLine, commaDelimited);
+		tokenizer::iterator value_iter = values.begin();
+		for (tokenizer::iterator key_iter = keys.begin();
+		key_iter != keys.end() && value_iter != values.end(); ++key_iter)
+		{
+			value = atof((*value_iter).c_str());
+			goals[*key_iter].push_back(value);
+			++value_iter;
+		}
+	}
+	goalInput.close();
+}
+void openPlan(string filename, moveit::planning_interface::MoveGroup::Plan plan)
+{
+	planOutput.open(filename.c_str());
+	planOutput << "time";
+	for(vector<string>::iterator it =  plan.trajectory_.joint_trajectory.joint_names.begin();
+			it !=  plan.trajectory_.joint_trajectory.joint_names.end(); it++) {
+		planOutput << "," << *it;
+	}
+	planOutput << "\n";
+}
+void closePlan()
+{
+	planOutput.close();
+}
+void savePlan(moveit::planning_interface::MoveGroup::Plan plan)
+{
+	double secs = 0;
+	for(vector<trajectory_msgs::JointTrajectoryPoint>::iterator pt =  plan.trajectory_.joint_trajectory.points.begin();
+			pt !=  plan.trajectory_.joint_trajectory.points.end(); pt++) {
+		secs = (*pt).time_from_start.toSec() / 4;
+		stringstream pointStream;
+		pointStream << secs + addTime;
+		for(vector<double>::iterator dt = (*pt).positions.begin(); dt != (*pt).positions.end(); dt++) {
+			pointStream << "," << *dt;
+		}
+		planOutput << pointStream.str() << "\n";
+	}
+	addTime += secs;
+}
